c8ef309cbb4fe73bac34a62828b2577a6046b8dd
Nguyễn Văn Dũng <nvdung.khtn@gmail.com>
2020-11-28 17:44:49 +0700

    header UI

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 // File điều hướng của ứng dụng
 const loginRouter = require('./accountRoute/login.js');
 const registerRouter = require('./accountRoute/register.js');
+const homeRouter = require('./home.js')
 
 function route(app) {
     app.use('/login', loginRouter);
     app.use('/register', registerRouter);
+    app.use('/', homeRouter);
 }
 
 module.exports = route;
