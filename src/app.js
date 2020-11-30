b0ec244105c111da3000d0f31024b2685bed62a6
Nguyễn Văn Dũng <nvdung.khtn@gmail.com>
2020-12-01 02:07:50 +0700

    Default error handler and handle the case that the url not exist

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const path = require('path');
+const numeral = require('numeral');
+require('express-async-errors');
+
 const PORT = 3000;
 const app = express();
 
@@ -32,6 +35,19 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Routes initialization
 route(app);
+
+app.use(function (req, res) {
+    res.render('vwErrors/404', {
+      layout: false
+    })
+});
+// default error handler
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.render('vwErrors/500', {
+      layout: false
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Sever is running at port ${PORT}......`);
