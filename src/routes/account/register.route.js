b7491a1de1bec928be999c6fa34e9588e2fa4daf
DESKTOP-C1J3814\AnhTuan <tuando24101997@gmail.com>
2020-12-11 15:06:00 +0700

    fontend page confirm otp register

--- src/routes/account/register.route.js
+++ src/routes/account/register.route.js
@@ -6,4 +6,10 @@ const registerController = require('../../controllers/account/registerController
 router.get('/', registerController.index);
 //router.post('/', registerController.register);
 
+router.get('/confirm', (req, res)=>{
+    res.render('vwAccount/confirmregister', {
+        layout: false,
+    });
+})
+
 module.exports = router;
