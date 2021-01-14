583b1685187c8182874c6cd5822582959270a2b9
phamvantrinh99 <trinhpham5337@gmail.com>
2021-01-14 12:24:22 +0700

    fix route detailCourse and videoCourse

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controllers/home/homeController');
+const courseController = require('../controllers/course/CourseController');
 
 router.get('/', homeController.index);
+router.get('/courses/:slug', courseController.show);
 
 
 module.exports = router;
